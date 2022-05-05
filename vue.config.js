a7fbc144f4c1b0a655b2af3188693065f0b4b06a
DingOUCer <dingyunlai@stu.ouc.edu.cn>
2022-05-05 23:35:52 +0800

    Basic page layout

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  css: {
+    loaderOptions: {
+      sass: {
+        // 全局引入变量和mixin
+        // additionalData相当于添加一些额外的sass文件
+        additionalData: `
+        @import "@/assets/scss/variable.scss";
+        @import "@/assets/scss/mixin.scss";
+        `
+      }
+    }
+  }
 })
